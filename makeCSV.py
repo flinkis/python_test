63f817f3a2290e197ebd07b4d7bae154d25a6934
Anders Flink <anders.flink@vaimo.com>
2016-02-01 19:41:50 +0100

    Changes to code after Emil and added cli input

--- makeCSV.py
+++ makeCSV.py
@@ -1,6 +1,5 @@
-
-import csv
-import os
+#!/usr/bin/env python
+import csv, os, sys, getopt
 
 def get_mods(file):
 	mods = {}
@@ -9,11 +8,11 @@ def get_mods(file):
 		for row in reader:
 			if len(row) > 1:
 				if row[0] not in mods:
-					mods[format_frase(row[0])] = list()
-				mods[format_frase(row[0])].append(row[3])
+					mods[format_phrase(row[0])] = list()
+				mods[format_phrase(row[0])].append(row[3])
 	return mods
 
-def format_frase(string):
+def format_phrase(string):
 	i = 1
 	while string.find('%s') >= 0:
 		string = string.replace('%s', '%'+str(i), 1)
@@ -35,38 +34,60 @@ def get_translations(directory):
 			reader = csv.reader(csvfile, delimiter=',', skipinitialspace='true')
 			for row in reader:	
 				if len(row) > 1:
-					translation[format_frase(row[0])] = format_frase(row[1])
+					translation[format_phrase(row[0])] = format_phrase(row[1])
 	
 	return translation
 
 def make_translation_list(mods, translation):
 	translation_list = []
-	for frase, trans in translation.items():
-		if frase in mods:
-			for mod in mods[frase]:
-				translation_list.append([frase, trans, 'module', mod])
+	for phrase, trans in translation.items():
+		if phrase in mods:
+			for mod in mods[phrase]:
+				translation_list.append([phrase, trans, 'module', mod])
 
 	return sorted(translation_list, key=lambda x: x[3])
 
 def make_untranslated_list(mods, translation):
 	untranslated_list = []
-	for mod in mods:
-		if mod not in translation:
-			untranslated_list.append(mod)
+	for phrase in mods:
+		if phrase not in translation:
+			for mod in mods[phrase]:
+				untranslated_list.append([phrase, phrase, 'module', mod])
 
-	print untranslated_list
+	return sorted(untranslated_list, key=lambda x: x[3])
 
-def write_new_csv(translation_list, out_filename):
+def write_new_csv(phrases_list, out_filename):
 	with open(out_filename, 'wb') as csvfile:
 		writer = csv.writer(csvfile, delimiter=',', doublequote='true', quoting=csv.QUOTE_NONNUMERIC)
-		for list_item in translation_list:
+		for list_item in phrases_list:
 			writer.writerow(list_item)
 
 def main():
-	mods = get_mods('magento2_frases.csv')
-	translation = get_translations('vaimo_italian/app/locale/it_IT/')
-	translation_list = make_translation_list(mods, translation)
-	write_new_csv(translation_list, 'it_IT.csv')
-	make_untranslated_list()
+	magento2_translation = 'magento2_phrases.csv'
+	translation_file_directory = ''
+	outpu_file_name = ''
 
-main()
+	try:
+		myopts, args = getopt.getopt(sys.argv[1:],"d:o:")
+	except getopt.GetoptError as e:
+		print (str(e))
+		print("Usage: %s -d input -o output " % sys.argv[0])
+		sys.exit(2)
+ 
+	for key, value in myopts:
+		if key == '-d':
+			translation_file_directory = value
+		elif key == '-o':
+			outpu_file_name = value
+
+	mods = get_mods(magento2_translation)
+	translation = get_translations(translation_file_directory)
+
+	translation_list = make_translation_list(mods, translation)
+	untranslated_list = make_untranslated_list(mods, translation)
+
+	write_new_csv(translation_list, outpu_file_name+'.csv')
+	write_new_csv(untranslated_list, outpu_file_name+'_untranslated.csv')
+	
+if __name__ == "__main__":
+	main()
